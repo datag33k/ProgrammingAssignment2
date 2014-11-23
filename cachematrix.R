fa2287cfd274cde8e1c6c2a3c2a8a8e9599ad0c8
Stacey Wransky <wransky@gmail.com>
2014-11-23 13:43:32 -0800

    Complete
    Final submit after comment tweaking.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
 ## Coursera R Programming Class: rprog-009
 ## Programming Assignment #2
-## Objective: Write two functions. One that will get/set data for a matrix and its inversion, 
-## and another will test if the inversion of a matrix is cached(stored) and if not, cache it.
+## Objective: Write two functions. One that will get/set data for a matrix and its inversion 
+## and another that will test if the inversion of a matrix is cached/stored and if not, cache it.
 
-## makeCacheMatrix - set/get a vector and its inverse 
+## makeCacheMatrix - set/get that data for a matrix and its inverse 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -19,7 +19,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheInverse - see if matrix "x" is cached/stored, if not, invert it and store it using the solve() function in R
+## cacheInverse - see if the inverse of matrix "x" is cached/stored. If not, invert and store it using the solve() function in R
 cacheInverse <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   m <- x$getInverse()
