7b5be13c12e013d77eb1ab480a72bd3f6ef391b6
Stacey Wransky <wransky@gmail.com>
2014-11-23 12:49:33 -0800

    first commit
    first commit after functions written

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R Programming Class: rprog-009
+## Programming Assignment #2
+## Objective: write 2 functions. One to test if the inversion of a matrix is cached, and if not, cache it.
+## The other function gets and sets the data for the matrix inversion.
 
-## Write a short comment describing this function
+## makeCacheMatrix - set/get a vector for caching
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(mean) m <<- mean
+  getSolve <- function() m
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - see if matrix is stored, if not, invert it and store it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getSolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setSolve(m)
+  m
 }
