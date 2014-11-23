3eda54e52dca0f21e0aa31a26dd58ba219290bbf
Stacey Wransky <wransky@gmail.com>
2014-11-23 13:38:51 -0800

    final final commit
    Ready for peer assessment

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,9 @@
 ## Coursera R Programming Class: rprog-009
 ## Programming Assignment #2
-## Objective: write 2 functions. One to test if the inversion of a matrix is cached, and if not, cache it.
-## The other function gets and sets the data for the matrix inversion.
+## Objective: Write two functions. One that will get/set data for a matrix and its inversion, 
+## and another will test if the inversion of a matrix is cached(stored) and if not, cache it.
 
-## makeCacheMatrix - set/get a vector for caching
-
+## makeCacheMatrix - set/get a vector and its inverse 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -12,25 +11,24 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setSolve <- function(solve) m <<- solve
-  getSolve <- function() m
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
   list(set = set, get = get,
-       setSolve = setSolve,
-       getSolve = getSolve)
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## cacheSolve - see if matrix is stored, if not, invert it and store it
-
-cacheSolve <- function(x, ...) {
+## cacheInverse - see if matrix "x" is cached/stored, if not, invert it and store it using the solve() function in R
+cacheInverse <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  m <- x$getSolve()
+  m <- x$getInverse()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
   data <- x$get()
   m <- solve(data, ...)
-  x$setSolve(m)
+  x$setInverse(m)
   m
 }
